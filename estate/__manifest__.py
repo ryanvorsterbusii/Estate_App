c0e5260c0ff63ef48f688ff8b00c12e438be7c9e
ryanvorsterbusii <ryan@busii.com>
2022-10-24 12:08:38 +0200

    test

---
+++ estate/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Real Estate App",
+
+    'summary': """
+        This is the estate module for developer How-Tos.... Created using a python integration""",
+
+    'description': """
+        This is a long description of module's purpose
+    """,
+
+    'author': "Ryan Vorster",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Estate App',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+
+    ],
+    # only loaded in demonstration mode
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'autoinstall': False
+
+}
