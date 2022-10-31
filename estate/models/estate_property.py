3d9722eda0d18e26cd1d4e93444f0506e8544c65
ryanvorsterbusii <ryan@busii.com>
2022-10-31 13:44:01 +0200

    Update

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,15 +1,16 @@
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
 
 
-class TestModel(models.Model):
+class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real Estate Property"
 
     name = fields.Char("Title", required=True)
     description = fields.Text("Description")
     postcode = fields.Char("Postcode")
-    date_availability = fields.Date("Available From", default=lambda self: self._default_date_availability(),
-                                    copy=False)
+    date_availability = fields.Date("Available From", default=lambda
+        self: self._default_date_availability(), copy=False)
     expected_price = fields.Float("Expected Price", required=True)
     selling_price = fields.Float("Selling Price", copy=False, readonly=True)
     bedrooms = fields.Integer("Bedrooms", default=2)
@@ -27,3 +28,21 @@ class TestModel(models.Model):
         ],
         string="Garden Orientation",
     )
+
+    state = fields.Selection(
+        selection=[
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("canceled", "Canceled"),
+        ],
+        string="Status",
+        required=True,
+        copy=False,
+        default="new",
+    )
+    active = fields.Boolean("Active", default=True)
+
+    def _default_date_availability(self):
+        return fields.Date.context_today(self) + relativedelta(months=3)
