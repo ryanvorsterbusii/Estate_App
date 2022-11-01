efacd0ac9d632b35036c3227df13906e67386422
ryanvorsterbusii <ryan@busii.com>
2022-11-01 14:16:42 +0200

    Update

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -3,6 +3,8 @@ from odoo import fields, models
 
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
-    _description = "Estate Property Type"
+    _description = "Real Estate Property Type"
 
     name = fields.Char("Name", required=True)
+
+    # property_ids = fields.One2many("estate.property", "property_type_id", string="Properties")
