fbf71227586223535803cf1f3184e782b8ebb8c5
ryanvorsterbusii <ryan@busii.com>
2022-11-01 16:07:41 +0200

    Update

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -6,5 +6,5 @@ class EstatePropertyType(models.Model):
     _description = "Real Estate Property Type"
 
     name = fields.Char("Name", required=True)
-
+    sequence = fields.Integer("Sequence", default=10)
     # property_ids = fields.One2many("estate.property", "property_type_id", string="Properties")
