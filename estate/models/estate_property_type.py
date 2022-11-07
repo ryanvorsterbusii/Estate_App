edeeb50ee6812fef76715c4a4c6a0a136b6436cf
ryanvorsterbusii <ryan@busii.com>
2022-11-07 13:29:36 +0200

    Update

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -7,7 +7,7 @@ class EstatePropertyType(models.Model):
     _sql_constraints = [
         ("check_name", "UNIQUE(name)", "The name must be unique"),
     ]
-    _name = "name"
+    _order = "name"
     name = fields.Char("Name", required=True)
     sequence = fields.Integer("Sequence", default=10)
     property_ids = fields.One2many("estate.property", "property_type_id", string="Properties")
