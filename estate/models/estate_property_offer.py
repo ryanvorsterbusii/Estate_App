f46c65ff426f5babc135f17ad7c769263f05c3d3
ryanvorsterbusii <ryan@busii.com>
2022-11-08 15:39:04 +0200

    Update

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -16,7 +16,22 @@ class EstatePropertyOffer(models.Model):
 
     price = fields.Float("Price", required=True)
     validity = fields.Integer(string="Validity (days)", default=7)
+
     date_deadline = fields.Date(string="Deadline", compute="_compute_date_deadline", inverse="_inverse_date_deadline")
+
+    @api.depends("create_date", "validity")
+    def _compute_date_deadline(self):
+        for offer in self:
+            date = offer.create_date.date() if offer.create_date else fields.Date.today()
+            offer.date_deadline = date + relativedelta(days=offer.validity)
+
+    def _inverse_date_deadline(self):
+        for offer in self:
+            date = offer.create_date.date() if offer.create_date else fields.Date.today()
+            offer.validity = (offer.date_deadline - date).days
+
+
+
 
     state = fields.Selection(
         selection=[
