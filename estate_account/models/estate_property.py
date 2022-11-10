34a7bf9907cc53b2f9b5852ba933e0ad10899128
ryanvorsterbusii <ryan@busii.com>
2022-11-10 13:58:54 +0200

    Added Estate account model

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -5,3 +5,24 @@ from odoo import models
 
 class EstateProperty(models.Model):
     _inherit = "estate.property"
+
+
+def action_sold(self):
+    soldaction = super().action_sold()
+    journal = self.env["account.journal"].search([("type", "=", "sale")])
+
+    for prop in self:
+        self.env["account.move"].create(
+            {
+                "partner_id": prop.buyer_id.id,
+                "move_type": "out_invoice",
+                "journal_id": journal.id,
+                "invoice_line_ids": [
+                    (
+
+                    )
+                ]
+
+            }
+        )
+    return soldaction
